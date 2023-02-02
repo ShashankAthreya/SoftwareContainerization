07c5fba3ffd6b246a2737ac26336a63de01e1a35
Anders Nõu <anders.nou@gmail.com>
2023-02-02 14:17:38 +0100

    Add ingress

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import TodoList from './components/TodoList'
 
-import axios from 'axios'
-
-axios.defaults.baseURL = process.env.VUE_APP_API_URL
-
 createApp(App).component('TodoList', TodoList).mount('#app')
