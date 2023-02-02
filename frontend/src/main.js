556526ddc182014c2ee40a0030cf365c8e5eab94
Anders Nõu <anders.nou@gmail.com>
2023-02-02 14:13:31 +0100

    Working API URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import TodoList from './components/TodoList'
 
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:5000/'
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 createApp(App).component('TodoList', TodoList).mount('#app')
